1c5eb75e553bbe4646323deee33825cb04e24ce2
Saumya101203 <118092711+Saumya101203@users.noreply.github.com>
2024-08-05 03:29:11 +0530

    Update institute_db.sql

--- institute_db.sql
+++ institute_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2024 at 09:46 PM
+-- Generation Time: Aug 04, 2024 at 11:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,7 +42,25 @@ INSERT INTO `notifications` (`id`, `project_id`, `user_email`, `owner_email`) VA
 (11, 5, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com'),
 (12, 5, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com'),
 (13, 7, 'spsingh101203@gmail.com', 'sp.singh.302101@gmail.com'),
-(14, 6, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com');
+(14, 6, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com'),
+(15, 8, 'spsingh101203@gmail.com', 'sp.singh.302101@gmail.com'),
+(16, 6, 'spsingh101203@gmail.com', 'spsingh101203@gmail.com'),
+(17, 9, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com'),
+(18, 5, 'spsingh101203@gmail.com', 'spsingh101203@gmail.com'),
+(19, 5, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com'),
+(20, 10, 'sp.singh.302101@gmail.com', 'sp.singh.302101@gmail.com'),
+(21, 14, 'sp.singh.302101@gmail.com', 'spsingh101203@gmail.com'),
+(22, 15, 'spsingh101203@gmail.com', 'sp.singh.302101@gmail.com'),
+(23, 16, 'sp.singh.302101@gmail.com', 'sp.singh.302101@gmail.com'),
+(24, NULL, NULL, NULL),
+(25, NULL, NULL, NULL),
+(26, NULL, NULL, NULL),
+(27, 17, 'spsingh101203@gmail.com', 'sp.singh.302101@gmail.com'),
+(28, 5, 'saumya@gmail.com', 'spsingh101203@gmail.com'),
+(29, 18, 'spsingh101203@gmail.com', 'saumya@gmail.com'),
+(30, 7, 'saumya@gmail.com', 'sp.singh.302101@gmail.com'),
+(31, 18, 'sp.singh.302101@gmail.com', 'saumya@gmail.com'),
+(32, 11, 'saumya@gmail.com', 'spsingh101203@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -66,7 +84,18 @@ CREATE TABLE `projects` (
 INSERT INTO `projects` (`id`, `email`, `projectName`, `projectSubject`, `projectDescription`, `projectTopics`) VALUES
 (5, 'spsingh101203@gmail.com', 'WEB', 'WEB', 'WEB', 'WEB'),
 (6, 'spsingh101203@gmail.com', 'ML', 'ML', 'ML', 'ML'),
-(7, 'sp.singh.302101@gmail.com', 'PHY', 'phy', 'phy', 'phy');
+(7, 'sp.singh.302101@gmail.com', 'PHY', 'phy', 'phy', 'phy'),
+(8, 'sp.singh.302101@gmail.com', 'Mat', 'Mat', 'Mat', 'Mat'),
+(9, 'spsingh101203@gmail.com', 'Dev', 'Dev', 'Dev', 'Dev'),
+(10, 'sp.singh.302101@gmail.com', 'Art', 'Art', 'Art', 'Art'),
+(11, 'spsingh101203@gmail.com', 'Chem', 'Chem', 'Chem', 'Chem'),
+(12, 'sp.singh.302101@gmail.com', 'bf d', 'jb j', 'm xm', 'xb c'),
+(13, 'sp.singh.302101@gmail.com', 'kdn', '  xm,', ' nxc', 'cx nncx'),
+(14, 'spsingh101203@gmail.com', 'AI&DS', 'AI', 'AI&DS', 'AI'),
+(15, 'sp.singh.302101@gmail.com', 'MME', 'MME', 'MME', 'MME'),
+(16, 'sp.singh.302101@gmail.com', 'JS', 'JS', 'JSSS', 'JS'),
+(17, 'sp.singh.302101@gmail.com', 'GFG', 'GFG', 'GFG', 'GFG'),
+(18, 'saumya@gmail.com', 'PHP', 'PHP', 'PHP', 'PHP');
 
 -- --------------------------------------------------------
 
@@ -86,7 +115,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `email`, `password`) VALUES
 (1, 'spsingh101203@gmail.com', 'Somvanshi@123'),
-(2, 'sp.singh.302101@gmail.com', 'password123');
+(2, 'sp.singh.302101@gmail.com', 'password123'),
+(3, 'saumya@gmail.com', '123456');
 
 --
 -- Indexes for dumped tables
@@ -119,19 +149,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
